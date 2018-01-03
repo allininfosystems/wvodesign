2145dcd448f40d7a3ab5f3446a6f858e8b57105d
allininfosystems <allin.akumar@gmail.com>
2018-01-03 06:02:12 -0500

    second commit

---
+++ app/etc/env.php
@@ -0,0 +1,72 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'a7f422dd0cd5fed65a9dd0389697c6ba',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'wvodesig',
+        'username' => 'wvodesig',
+        'password' => 'tr2n6Qtbq9RmdRUC',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Fri, 15 Sep 2017 07:37:29 +0000',
+  ),
+  'system' => 
+  array (
+    'default' => 
+    array (
+      'dev' => 
+      array (
+        'debug' => 
+        array (
+          'debug_logging' => '0',
+        ),
+      ),
+    ),
+  ),
+);
